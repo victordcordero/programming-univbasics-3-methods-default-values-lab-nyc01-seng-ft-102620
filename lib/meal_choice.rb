fbf9a3b5237bf0db012edb3ddeff469780458f8a
Victor Cordero <victordcordero@gmail.com>
2020-10-18 21:25:39 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,6 +6,8 @@ def meal_choice(veg1,veg2,protein = "tofu")
   p "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
+meal_choice
+
 meal_choice("broccoli", "macaroni")
 
 meal_choice("broccoli", "macaroni", "roast beef")
